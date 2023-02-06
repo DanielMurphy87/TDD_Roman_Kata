68f344078e6b21ec06f9171e66567b9d3910a41b
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-02-06 23:35:09 +0200

    Add simple logic and tests

--- src/roman.js
+++ src/roman.js
@@ -1,1 +1,27 @@
+function numToRoman(num) {
+    if (typeof num !== 'number') throw new Error('Input must be a number');
+    if (num < 0) throw new Error('Input must be a positive number');
+
+    let result = '';
+
+    while (num > 0) {
+        switch (true) {
+            case num >= 5:
+                result += 'V';
+                num -= 5;
+                break;
+            case num >= 4:
+                result += 'IV';
+                num -= 4;
+                break;
+            case num >= 1:
+                result += 'I';
+                num -= 1;
+                break;
+
+        }
+    }
+    return result;
+}
+
 export { numToRoman }
--- src/roman.test.js
+++ src/roman.test.js
@@ -2,4 +2,13 @@ import { numToRoman } from './roman';  //using the ES6 modules
 
 test ('should return the correct Roman numeral for the given integer', () => {
   expect(numToRoman(1)).toBe("I");
+  expect(numToRoman(2)).toBe("II");
+  expect(numToRoman(5)).toBe("V");
+  expect(numToRoman(4)).toBe("IV");
+  expect(numToRoman(5)).toBe("V");
 });
+
+test ('should return an error if not a string is passed in', () =>{
+  expect(() => numToRoman('string')).toThrowError("Input must be a number");
+  expect(() => numToRoman(-1)).toThrowError("Input must be a positive number");
+})
