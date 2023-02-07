608687d36200eb3c887e4f64604e72bfa17fee98
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-02-07 12:30:53 +0200

    Finish converting code and add more tests

--- src/roman.js
+++ src/roman.js
@@ -6,6 +6,46 @@ function numToRoman(num) {
 
     while (num > 0) {
         switch (true) {
+            case num >= 1000:
+                result += 'M';
+                num -= 1000;
+                break;
+            case num >= 900:
+                result += 'CM';
+                num -= 900;
+                break;
+            case num >= 500:
+                result += 'D';
+                num -= 500;
+                break;
+            case num >= 400:
+                result += 'CD';
+                num -= 400;
+                break;
+            case num >= 100:
+                result += 'C';
+                num -= 100;
+                break;
+            case num >= 90:
+                result += 'XC';
+                num -= 90;
+                break;
+            case num >= 50:
+                result += 'L';
+                num -= 50;
+                break;
+            case num >= 40:
+                result += 'XL';
+                num -= 40;
+                break;
+            case num >= 10:
+                result += 'X';
+                num -= 10;
+                break;
+            case num >= 9:
+                result += 'IX';
+                num -= 9;
+                break;
             case num >= 5:
                 result += 'V';
                 num -= 5;
@@ -18,7 +58,6 @@ function numToRoman(num) {
                 result += 'I';
                 num -= 1;
                 break;
-
         }
     }
     return result;
--- src/roman.test.js
+++ src/roman.test.js
@@ -1,14 +1,17 @@
 import { numToRoman } from './roman';  //using the ES6 modules 
 
-test ('should return the correct Roman numeral for the given integer', () => {
+test('should return the correct Roman numeral for the given integer', () => {
   expect(numToRoman(1)).toBe("I");
   expect(numToRoman(2)).toBe("II");
   expect(numToRoman(5)).toBe("V");
   expect(numToRoman(4)).toBe("IV");
   expect(numToRoman(5)).toBe("V");
+  expect(numToRoman(9)).toBe('IX');
+  expect(numToRoman(58)).toBe('LVIII');
+  expect(numToRoman(1994)).toBe('MCMXCIV');
 });
 
-test ('should return an error if not a string is passed in', () =>{
+test('should return an error if not a string is passed in', () => {
   expect(() => numToRoman('string')).toThrowError("Input must be a number");
   expect(() => numToRoman(-1)).toThrowError("Input must be a positive number");
 })
