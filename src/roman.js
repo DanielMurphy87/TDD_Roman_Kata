c6e3124bd2d999dc6e4548403d56205c57614c10
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-02-13 17:34:02 +0200

    update return variable name to be more informative

--- src/roman.js
+++ src/roman.js
@@ -2,65 +2,65 @@ function numToRoman(num) {
     if (typeof num !== 'number') throw new Error('Input must be a number');
     if (num < 0) throw new Error('Input must be a positive number');
 
-    let result = '';
+    let numeral = '';
 
     while (num > 0) {
         switch (true) {
             case num >= 1000:
-                result += 'M';
+                numeral += 'M';
                 num -= 1000;
                 break;
             case num >= 900:
-                result += 'CM';
+                numeral += 'CM';
                 num -= 900;
                 break;
             case num >= 500:
-                result += 'D';
+                numeral += 'D';
                 num -= 500;
                 break;
             case num >= 400:
-                result += 'CD';
+                numeral += 'CD';
                 num -= 400;
                 break;
             case num >= 100:
-                result += 'C';
+                numeral += 'C';
                 num -= 100;
                 break;
             case num >= 90:
-                result += 'XC';
+                numeral += 'XC';
                 num -= 90;
                 break;
             case num >= 50:
-                result += 'L';
+                numeral += 'L';
                 num -= 50;
                 break;
             case num >= 40:
-                result += 'XL';
+                numeral += 'XL';
                 num -= 40;
                 break;
             case num >= 10:
-                result += 'X';
+                numeral += 'X';
                 num -= 10;
                 break;
             case num >= 9:
-                result += 'IX';
+                numeral += 'IX';
                 num -= 9;
                 break;
             case num >= 5:
-                result += 'V';
+                numeral += 'V';
                 num -= 5;
                 break;
             case num >= 4:
-                result += 'IV';
+                numeral += 'IV';
                 num -= 4;
                 break;
             case num >= 1:
-                result += 'I';
+                numeral += 'I';
                 num -= 1;
                 break;
         }
     }
-    return result;
+    return numeral;
 }
 
 export { numToRoman }
